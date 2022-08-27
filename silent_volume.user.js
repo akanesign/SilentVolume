c1de1d475a4b35532404ecbb75a9253bb2c13fd4
akanesign <110755602+akanesign@users.noreply.github.com>
2022-08-28 04:31:18 +0900

    first commit

---
+++ silent_volume.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Silent Volume
+// @version      0.1
+// @description  Please be quiet in the room
+// @author       akanesign
+// @match        https://tweetdeck.twitter.com/
+// @match        https://www.youtube.com/watch?*
+// @match        https://youtube.com/watch?*
+// @match        https://twitter.com/*
+// @match        https://twitter.com/*/*/*
+// @match        https://video.twimg.com/ext_tw_video/*/*/*/*/*
+// @require      https://code.jquery.com/jquery-3.5.1.min.js
+// ==/UserScript==
+var volume_default = 0.25;
+
+function set_volumes() {
+  var target = document.getElementsByTagName("video");
+  for( var cnt = 0; cnt < target.length; cnt++ ) {
+     target[ cnt ].volume = volume_default;
+  }
+  setTimeout( set_volumes, 1000 );
+}
+
+$(document).ready(function(){
+  setTimeout( set_volumes, 1000 );
+})();
