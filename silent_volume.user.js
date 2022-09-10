920f433a826130beaa09bb08086131515ac5ef24
akanesign <110755602+akanesign@users.noreply.github.com>
2022-09-10 16:07:16 +0900

    slider

--- silent_volume.user.js
+++ silent_volume.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Silent Volume
-// @version      0.5
+// @version      0.6
 // @description  Please be quiet in the room
 // @author       akanesign
 // @match        https://tweetdeck.twitter.com/
@@ -27,6 +27,8 @@
 // @match        https://www.tiktok.com/*/*/*
 // @match        https://www.instagram.com/
 // @match        https://www.instagram.com/*/*
+// @grant        GM_getValue
+// @grant        GM_setValue
 // @require      https://code.jquery.com/jquery-3.5.1.min.js
 // @updateURL    https://github.com/akanesign/SilentVolume/raw/main/silent_volume.user.js
 // @downloadURL  https://github.com/akanesign/SilentVolume/raw/main/silent_volume.user.js
@@ -36,11 +38,39 @@ var volume_default = 0.25;
 function set_volumes() {
   var date = new Date();
   var time = date.getHours();
+  var opt_Volume = 0.25;
 
-  if ( time > 20 || time < 9 ) {
+  if ( time > 23 || time < 9 ) {
+    if ( GM_getValue("opt_Volume") != undefined ) opt_Volume = GM_getValue("opt_Volume");
+
     var target = document.getElementsByTagName("video");
     for( var cnt = 0; cnt < target.length; cnt++ ) {
-       target[ cnt ].volume = volume_default;
+       var pr = target[ cnt ].parentNode;
+       target[ cnt ].volume = opt_Volume;
+
+       if( pr.querySelectorAll(".silent-volume").length == 0 ) {
+         var dl = document.createElement("div");
+         dl.style.height = target[ cnt ].style.height;
+         dl.style.width = target[ cnt ].style.width;
+
+         var vl = document.createElement("input");
+         vl.setAttribute("type", "range");
+         vl.setAttribute("title", "âπó êßå¿:" + opt_Volume * 100 + "%");
+         vl.setAttribute("class", "silent-volume");
+         vl.setAttribute("min", "0");
+         vl.setAttribute("max", "100");
+         vl.setAttribute("size", "10");
+         vl.setAttribute("value", opt_Volume * 100);
+         vl.setAttribute("step", "5");
+         vl.setAttribute("style", "right:0; position:absolute; margin:20px; width:100px; z-index:100;")
+
+         vl.addEventListener('change', (event) => {
+           GM_setValue( "opt_Volume", event.target.value/100 );
+           event.target.title = "âπó êßå¿:" + event.target.value + "%";
+         });
+         dl.append(vl);
+         pr.append(dl);
+       }
     }
   }
   setTimeout( set_volumes, 1000 );
